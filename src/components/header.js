b8d6e2ab40c91e6f5ba6d67e59f275c2e0f8fc88
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-10-04 17:09:33 +0200

    action creator hookup

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
+import * as actions from '../actions';
 
-export default class Header extends Component {
+class Header extends Component {
     authButton(){
-        return <button>Sign In</button>
+        if (this.props.authenticated) {
+            return <button onClick={() => this.props.authenticate(false)}>Sing Out</button>
+        }
+        return <button onClick={() => this.props.authenticate(true)}>Sign In</button>
     }
     render() {
         return (
@@ -23,3 +28,10 @@ export default class Header extends Component {
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+    authenticated: state.authenticated
+})
+
+
+export default connect(mapStateToProps, actions)(Header);
