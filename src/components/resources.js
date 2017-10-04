904fc6e05eb64fb0b7124df1cdb36d0c1f4be52e
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-10-04 16:18:13 +0200

    react-roter set up

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () =>  {
+    return (
+        <div>
+            <h4>Fayuelos Secrect Recipt</h4>
+            <ul>
+                <li>1/2 Cup of Sugar</li>
+                <li>500ml of Milk</li>
+                <li>2 Eggs</li>
+                <li>80gr of flour</li>
+            </ul>
+        </div>
+    )
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/app';
+import Resources from './components/resources';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path='/resources' component={Resources}/>
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
