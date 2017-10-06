597f734a2dc1eee1e3875a5274eab466f4c0d6ae
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-10-06 14:25:01 +0200

    nesting HOCs

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
         render() {
+            console.log(this.props.authenticated);
             return <ComposedComponent {...this.props} />
         }
     }
-    
-    return Authentication;
+
+    const mapStateToProps = (state) => ({
+         authenticated: state.authenticated
+    })
+
+    return connect(mapStateToProps)(Authentication);
 }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import { logger } from 'redux-logger';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
 export default createStoreWithMiddleware(reducers);
